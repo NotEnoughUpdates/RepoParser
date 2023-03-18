75ac7c973fed68d371945b4252c614465614f9cb
nea <nea@nea.moe>
2023-03-18 15:48:18 +0100

    Add deprecation warning

--- src/main/java/io/github/moulberry/repo/data/NEURecipe.java
+++ src/main/java/io/github/moulberry/repo/data/NEURecipe.java
@@ -6,6 +6,7 @@ public interface NEURecipe {
     /**
      * This is to be called once, after deserialization by the item that contains this recipe. This will be done
      * automatically by {@link NEUItem#getRecipes()}.
+     * This method is deprecated because it should only be called by the Repository itself, not consumers of this library.
      */
     @Deprecated
     default void fillItemInfo(NEUItem item) {
